309df4c7878eb152d13c53939b9f3c06e07dc8cd
Alondra <alondracha04@gmail.com>
2025-03-11 14:18:09 -0600

    Boms&Tronco

--- script.js
+++ script.js
@@ -35,10 +35,9 @@ function preload () {
     this.load.image('sky', 'assets/Fondo.webp');
     this.load.image('ground', 'assets/tronco.png');
     this.load.image('star', 'assets/bill.png');
-    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('bomb', 'assets/Hectorgon.png');
     this.load.image('vida', 'assets/vida.png');
-    this.load.spritesheet('dude', 'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('dude', 'assets/Mabelonga.png', { frameWidth: 66, frameHeight: 80 });
 }
 
 // Función para crear los elementos del juego
@@ -53,50 +52,55 @@ function create () {
     //Creacion de las plataformas
     platforms.create(240, 1000, 'ground').setScale(2).refreshBody();
     platforms.create(800, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();;
-    platforms.create(1400, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();;
+    platforms.create(1400, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();; /*Troncos gdes de abajo*/
     platforms.create(2000, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();;
-    platforms.create(100, 400, 'ground');
-    platforms.create(600, 700, 'ground');
-    platforms.create(1000, 300, 'ground').setScale(1.5).refreshBody();
-    platforms.create(1500, 480, 'ground');
-    platforms.create(1750, 700, 'ground');
+    platforms.create(100, 600, 'ground').setScale(1).setFlipX(true).refreshBody();;; /*Lo ando moviendo yo */
+    platforms.create(600, 700, 'ground').setScale(0.5).setFlipX(true).refreshBody();
+    platforms.create(900, 800, 'ground').setScale(0.75).setFlipX(true).refreshBody();
+    platforms.create(600, 450, 'ground').setScale(0.75).setFlipX(true).refreshBody();
+    platforms.create(1200, 600, 'ground').setScale(1).setFlipX(true).refreshBody();
+    platforms.create(1000, 300, 'ground').setScale(1).setFlipX(true).refreshBody();
+    platforms.create(1500, 450, 'ground').setScale(0.5).setFlipX(true).refreshBody();
+    platforms.create(1750, 700, 'ground').setScale(0.5).setFlipX(true).refreshBody();
+    platforms.create(1500, 750, 'ground').setScale(0.75).setFlipX(true).refreshBody();
     
     // Jugador
-    player = this.physics.add.sprite(100, 450, 'dude');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+player = this.physics.add.sprite(100, 750, 'dude');
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
 
-    // Animaciones del jugador
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
+// Animaciones del jugador
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 2, end: 7 }),
+    frameRate: 10,
+    repeat: -1
+});
 
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
+this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 8 }],
+    frameRate: 20
+});
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 9, end: 15 }),
+    frameRate: 10,
+    repeat: -1
+});
 
-    // Estrellas
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
+// Estrellas
+stars = this.physics.add.group({
+    key: 'star',
+    repeat: 20,
+    setXY: { x: 60, y: 0, stepX: 90 }
+});
 
-    stars.children.iterate(function (child) {
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
+stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+});
+
 
     // Bombas
     bombs = this.physics.add.group();
@@ -154,6 +158,7 @@ function collectStar (player, star) {
         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
         var bomb = bombs.create(x, 16, 'bomb');
         bomb.setBounce(1);
+        bomb.setScale(0.1);
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
         bomb.allowGravity = false;
