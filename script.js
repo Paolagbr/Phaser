5c5f135c86944e1bedbfd88804e13761b6f20b31
Alondra <alondracha04@gmail.com>
2025-03-14 18:05:13 -0600

    CorreciónDeJuego

--- script.js
+++ script.js
@@ -24,68 +24,82 @@ var config = {
 var game = new Phaser.Game(config);
 
 // Variables globales
-var player, stars, bombs, platforms, cursors;
-var vidas = 3;
-var vidasI = []; // Almacena las imágenes de las vidas
+var player;
+var stars;
+var vidas=3;
+var bombs;
+var platforms;
+var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
+var vidas = 3;
+var vidasI = [];//Almacena las imagenes de las vidas
 
 // Función para precargar los recursos
-function preload() {
+function preload () {
     this.load.image('sky', 'assets/Fondo.webp');
     this.load.image('ground', 'assets/tronco.png');
     this.load.image('star', 'assets/bill.png');
     this.load.image('bomb', 'assets/Hectorgon.png');
     this.load.image('vida', 'assets/vida.png');
 
-    // Cargar el sprite del personaje seleccionado
-    let spritePath = "";
-    if (personajeSeleccionado === "Mabelonga") {
-        spritePath = 'assets/Mabelonga.png';
-    } else if (personajeSeleccionado === "Dipper") {
-        spritePath = 'assets/Dippersi.png';
+    // Cargar el sprite del personaje seleccionado usando los ids "drag1" y "drag2"
+    if (personajeSeleccionado === "drag1") {
+        this.load.spritesheet('dude', 'assets/Mabelonga.png', { frameWidth: 66, frameHeight: 80 });
+    } else if (personajeSeleccionado === "drag2") {
+        this.load.spritesheet('dude', 'img/Adobe.png', { frameWidth: 43, frameHeight: 80 });
     }
 
-    if (spritePath) {
-        this.load.spritesheet('dude', spritePath, { frameWidth: 66, frameHeight: 80 });
-    } else {
-        console.error("Personaje no encontrado. Se usará 'drag1' por defecto.");
-        this.load.spritesheet('dude', 'assets/drag1.png', { frameWidth: 66, frameHeight: 80 });
-    }
-
-    console.log("Personaje cargado en el juego:", personajeSeleccionado);
+    console.log("Personaje cargado en el juego: ", personajeSeleccionado);
 }
 
 // Función para crear los elementos del juego
-function create() {
+function create () {
     // Fondo
     var sky = this.add.image(0, 0, 'sky').setOrigin(0, 0);
-    sky.setScale(2);
+    sky.setScale(2);//Se muestra el fondo de pantalla
+
 
     // Plataformas
     platforms = this.physics.add.staticGroup();
+    //Creacion de las plataformas
     platforms.create(240, 1000, 'ground').setScale(2).refreshBody();
-    platforms.create(800, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();
-    platforms.create(1400, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();
-    platforms.create(100, 600, 'ground').setScale(1).setFlipX(true).refreshBody();
+    platforms.create(800, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();;
+    platforms.create(1400, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();; /*Troncos gdes de abajo*/
+    platforms.create(2000, 1000, 'ground').setScale(2).setFlipX(true).refreshBody();;
+    platforms.create(100, 600, 'ground').setScale(1).setFlipX(true).refreshBody();;; /*Lo ando moviendo yo */
     platforms.create(600, 700, 'ground').setScale(0.5).setFlipX(true).refreshBody();
-
-    // Jugador
+    platforms.create(900, 800, 'ground').setScale(0.75).setFlipX(true).refreshBody();
+    platforms.create(600, 450, 'ground').setScale(0.75).setFlipX(true).refreshBody();
+    platforms.create(1200, 600, 'ground').setScale(1).setFlipX(true).refreshBody();
+    platforms.create(1000, 300, 'ground').setScale(1).setFlipX(true).refreshBody();
+    platforms.create(1500, 450, 'ground').setScale(0.5).setFlipX(true).refreshBody();
+    platforms.create(1750, 700, 'ground').setScale(0.5).setFlipX(true).refreshBody();
+    platforms.create(1500, 750, 'ground').setScale(0.75).setFlipX(true).refreshBody();
+    
+        // Jugador
     player = this.physics.add.sprite(100, 750, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
     configurarAnimaciones(this, personajeSeleccionado);
 
-    // Estrellas
+    // Animaciones del jugador
+    //Estrellas
     stars = this.physics.add.group({
         key: 'star',
         repeat: 1,
         setXY: { x: 60, y: 0, stepX: 90 }
     });
 
-    stars.children.iterate(child => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)));
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    stars.children.iterate(function(child){
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
 
     // Bombas
     bombs = this.physics.add.group();
@@ -98,50 +112,71 @@ function create() {
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
     this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    // Solapamiento entre el jugador y las estrellas
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
     // Controles del cursor
     cursors = this.input.keyboard.createCursorKeys();
 
-    // Mostrar vidas del jugador
+    //Se muestran las vidas del jugador
+    
     for (var i = 0; i < vidas; i++) {
         var vidaImagen = this.add.image(20 + (i * 30), 100, 'vida').setScale(0.8);
         vidasI.push(vidaImagen);
     }
-}
 
-// Configurar animaciones según el personaje
-function configurarAnimaciones(scene, personaje) {
-    let frameStart = 2, frameEnd = 7; // Por defecto
-
-    if (personaje === "Dipper") {
-        frameStart = 4;
-        frameEnd = 7;
+    configurarAnimaciones(this, personajeSeleccionado)
+        cursors = this.input.keyboard.createCursorKeys();
     }
 
-    scene.anims.create({
-        key: 'left',
-        frames: scene.anims.generateFrameNumbers('dude', { start: frameStart, end: frameEnd }),
-        frameRate: 10,
-        repeat: -1
-    });
+    //Configurar animaciones según el personaje
+    function configurarAnimaciones(scene, personaje){
+        if(personaje === "drag1"){
+            scene.anims.create({
+                key: 'left',
+                frames: scene.anims.generateFrameNumbers('dude', {start: 2, end: 7}),
+                frameRate: 10,
+                repeat: -1
+            });
 
-    scene.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 8 }],
-        frameRate: 20
-    });
+            scene.anims.create({
+                key: 'turn',
+                frames:[{key: 'dude', frame: 8}],
+                frameRate: 20,
+            });
 
-    scene.anims.create({
-        key: 'right',
-        frames: scene.anims.generateFrameNumbers('dude', { start: 9, end: 15 }),
-        frameRate: 10,
-        repeat: -1
-    });
+            scene.anims.create({
+                key: 'right',
+                frames: scene.anims.generateFrameNumbers('dude', { start: 9, end: 15 }),
+                frameRate: 10,
+                repeat: -1
+            });
+        } else if(personaje === "drag2") {
+            scene.anims.create({
+                key: 'left',
+                frames: scene.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            scene.anims.create({
+                key: 'turn',
+                frames: [{ key: 'dude', frame: 8 }],
+                frameRate: 20
+            });
+
+            scene.anims.create({
+                key: 'right',
+                frames: scene.anims.generateFrameNumbers('dude', { start: 9, end: 12 }),
+                frameRate: 10,
+                repeat: -1
+            });
+    }
 }
 
 // Función de actualización
-function update() {
+function update () {
     if (gameOver) return;
 
     if (cursors.left.isDown) {
@@ -161,17 +196,19 @@ function update() {
 }
 
 // Función para recolectar estrellas
-function collectStar(player, star) {
+function collectStar (player, star) {
     star.disableBody(true, true);
 
     score += 10;
     scoreText.setText('Puntuación: ' + score);
 
     if (stars.countActive(true) === 0) {
-        stars.children.iterate(child => child.enableBody(true, child.x, 0, true, true));
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
 
-        let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-        let bomb = bombs.create(x, 16, 'bomb');
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        var bomb = bombs.create(x, 16, 'bomb');
         bomb.setBounce(1);
         bomb.setScale(0.1);
         bomb.setCollideWorldBounds(true);
@@ -181,16 +218,26 @@ function collectStar(player, star) {
 }
 
 // Función para manejar la colisión con bombas y vidas
-function hitBomb(player, bomb) {
-    let playerX = player.x;
-    let playerY = player.y;
+function hitBomb (player, bomb) {
+    var playerX = player.x;//El jugador se mantega en su posicion 
+    var playerY = player.y;
 
     player.setTint(0xff0000);
     player.anims.play('turn');
     perderVida.call(this, playerX, playerY);
     bomb.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
-}
 
+   /* if(vidas<=3){
+        vidas--;
+        player.setTint(0xff0000);
+
+    }else if(vidas===0){
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        gameOver = true;
+    }*/
+}
 function perderVida(playerX, playerY) {
     vidas--;
     if (vidas >= 0) {
